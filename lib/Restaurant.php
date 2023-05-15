0b28559407d0f3af80abe22f27e33e34e155b0e1
Hans De Beuckelaer <hans@tablebooker.be>
2023-05-15 15:05:37 +0200

    Make it possible to send request data (e.g. source, partner) with the restaurant retrieve function.

--- lib/Restaurant.php
+++ lib/Restaurant.php
@@ -9,16 +9,9 @@ namespace Tablebooker;
  */
 class Restaurant extends ApiResource
 {
-    public static function retrieve($id, $options = null) {
-        $url = static::classUrl();
-        $url .= "/info/" . $id;
-        $params = array();
-
-        $opts = Http\RequestOptions::parse($options);
-        $requestor = new ApiRequestor($opts->apiKey);
-        $response = $requestor->request('get', $url, $params, $opts->headers, null);
-
-        return $response;
+    public static function retrieve(Request\RestaurantInfo $requestData, $options = null) {
+        $url = static::classUrl() . "/info";
+        return self::_staticRequest('get', $url, $requestData, $options);
     }
 
     public static function sendMessage(Request\RestaurantSendMessage $requestData, Auth\AuthBasic $auth, $options = null)
