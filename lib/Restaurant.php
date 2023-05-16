8ea7dc6a1e80bdbb59e604b33a59b13840ed84c4
Hans De Beuckelaer <hans@tablebooker.be>
2023-05-16 12:47:58 +0200

    Refactor previous change so that it becomes a minor instead of a major change.

--- lib/Restaurant.php
+++ lib/Restaurant.php
@@ -2,6 +2,8 @@
 
 namespace Tablebooker;
 
+use Tablebooker\Request\RestaurantInfo;
+
 /**
  * Class Restaurant
  *
@@ -9,8 +11,23 @@ namespace Tablebooker;
  */
 class Restaurant extends ApiResource
 {
-    public static function retrieve(Request\RestaurantInfo $requestData, $options = null) {
+    /**
+     * Call the restaurant info api. Retrieves all restaurant settings.
+     *
+     * @param Request\RestaurantInfo|string $requestData    The obfuscated restaurant is as a String or a RequestInfo object containing all request data.
+     * @param array|null $options    Array containing all necessary options needed for this request as key => value pairs
+     * @return ApiResponse
+     * @throws Error\ApiError
+     * @throws Error\AuthenticationError
+     * @throws Error\InvalidRequest
+     */
+    public static function retrieve($requestData, $options = null) {
         $url = static::classUrl() . "/info";
+
+        if (gettype($requestData) === 'string'){
+            $requestData = new RestaurantInfo($requestData);
+        }
+
         return self::_staticRequest('get', $url, $requestData, $options);
     }
 
